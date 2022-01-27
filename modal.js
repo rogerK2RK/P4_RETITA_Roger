4e98085816c32f75a0ee63c64f45df799a7e60cf
Roger Retita <roger.retita@gmail.com>
2022-01-27 09:06:30 +0100

    creation de la classe utilisateur

--- modal.js
+++ modal.js
@@ -20,4 +20,43 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//declaration d'un booleen pour le test final
+let istValid = true;
 
+//objet pour chaque utilisateur
+class compteUtilisateur {
+  constructor(nom, prenom, email, dateNaissance){
+    this.nom = nom;
+    this.prenom = prenom;
+    this.email = email;
+    this.dateNaissance = dateNaissance;
+  }
+
+}
+
+//on récupère l'élément sur lequel on veut vérifier "exemple compter le nombre de lettre"
+/*compteUtilisateur.nom = document.getElementById('last');
+compteUtilisateur.prenom = document.getElementById('first');
+compteUtilisateur.email = document.getElementById('email');
+compteUtilisateur.dateNaissance = document.getElementById('birthdate');*/
+
+let unUtilisateur = new compteUtilisateur("RETITA", "Roger", "roger.retita@gmail.com", "10 mai 1999");
+console.log(unUtilisateur);
+
+//compter le nombre de lettre
+function getLetterCount(nom, nombreCaractere){
+ let nombre = nom.value.length;
+ nombreCaractere.innerHTML = nombre;
+ /*let totalLetters = 0;
+ for (let word of nom){
+   totalLetters += word.length;
+ }
+ console.log("le nom fait " + totalLetters + "lettre"  );
+ if(totalLetters < 3){
+  istValid = false;
+  console.log("Veullez faire une saisie plus de 3 lettre");
+}else{
+ console.log("Votre saisi est valable");
+}*/
+}
+getLetterCount(this.nom);
