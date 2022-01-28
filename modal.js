395158e42ca0f9cb257a1f8b6b0990c98444b6ed
Roger Retita <roger.retita@gmail.com>
2022-01-28 14:22:26 +0100

    verification du nom prenom et email

--- modal.js
+++ modal.js
@@ -21,18 +21,58 @@ function launchModal() {
 }
 
 //declaration d'un booleen pour le test final
-let istValid = true;
+let isValid = true;
 
-//objet pour chaque utilisateur
-class compteUtilisateur {
-  constructor(nom, prenom, email, dateNaissance){
-    this.nom = nom;
-    this.prenom = prenom;
-    this.email = email;
-    this.dateNaissance = dateNaissance;
+//Verifi le prenom
+let prenomUser = document.getElementById("first");
+prenomUser.addEventListener("input", function(e){
+  let prenomMessage = document.getElementById("prenom-validation");
+  if(prenomUser.value.length < 4){
+    prenomMessage.innerText = "Nom invalide; Doit faire plus de 3 caractère";
+    isValid = false;
+  }else{
+    prenomMessage.innerText = ""; 
   }
+});
 
-}
+//Verifie le nom
+let nomUser  = document.getElementById("last");
+nomUser.addEventListener("input", function(e){
+  let nomMessage = document.getElementById("nom-validation");
+  if(nomUser.value.length < 4){
+    nomMessage.innerText = "Nom invalide; Doit faire plus de 3 caractère";
+    isValid = false;
+  }else{
+    nomMessage.innerText = "";
+  }
+});
+
+//verifie le email
+let emailUser = document.getElementById("email");
+emailUser.addEventListener("input", function(e){
+  let emailMessage = document.getElementById("email-validation");
+  if(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(emailUser.value)){
+    emailMessage.innerText = "Valider";
+  }else{
+    emailMessage.innerText = "veuiller saisir un mail";
+    isValid = false;
+  }
+});
+
+//verification de la date de naissance
+let jeuneUser = "28-01-2010";
+let deadUser = "28-01-1922";
+let birthdateUser = document.getElementById("birthdate");
+birthdateUser.addEventListener("input", function(e){
+  let birthdateMessage = document.getElementById("birthdate-valitaion");
+  if(birthdateUser.value < jeuneUser){
+    birthdateMessage.innerText = "Vous êtes trops jeune !";
+    isValid = false;
+  }else{
+    birthdateMessage.innerText = " Vous avez l'âge pour participer !";
+  }
+})
+
 
 //on récupère l'élément sur lequel on veut vérifier "exemple compter le nombre de lettre"
 /*compteUtilisateur.nom = document.getElementById('last');
@@ -40,23 +80,3 @@ compteUtilisateur.prenom = document.getElementById('first');
 compteUtilisateur.email = document.getElementById('email');
 compteUtilisateur.dateNaissance = document.getElementById('birthdate');*/
 
-let unUtilisateur = new compteUtilisateur("RETITA", "Roger", "roger.retita@gmail.com", "10 mai 1999");
-console.log(unUtilisateur);
-
-//compter le nombre de lettre
-function getLetterCount(nom, nombreCaractere){
- let nombre = nom.value.length;
- nombreCaractere.innerHTML = nombre;
- /*let totalLetters = 0;
- for (let word of nom){
-   totalLetters += word.length;
- }
- console.log("le nom fait " + totalLetters + "lettre"  );
- if(totalLetters < 3){
-  istValid = false;
-  console.log("Veullez faire une saisie plus de 3 lettre");
-}else{
- console.log("Votre saisi est valable");
-}*/
-}
-getLetterCount(this.nom);
