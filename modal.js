4a1925f2f96832c7d2d69e4b8164183653ea95e4
Roger Retita <roger.retita@gmail.com>
2022-02-01 17:37:06 +0100

    birthdate check

--- modal.js
+++ modal.js
@@ -28,7 +28,7 @@ let prenomUser = document.getElementById("first");
 prenomUser.addEventListener("input", function(e){
   let prenomMessage = document.getElementById("prenom-validation");
   if(prenomUser.value.length < 4){
-    prenomMessage.innerText = "Nom invalide; Doit faire plus de 3 caractère";
+    prenomMessage.innerText = "Prénom doit faire plus de 3 caractère !";
     isValid = false;
   }else{
     prenomMessage.innerText = ""; 
@@ -40,7 +40,7 @@ let nomUser  = document.getElementById("last");
 nomUser.addEventListener("input", function(e){
   let nomMessage = document.getElementById("nom-validation");
   if(nomUser.value.length < 4){
-    nomMessage.innerText = "Nom invalide; Doit faire plus de 3 caractère";
+    nomMessage.innerText = "Nom doit faire plus de 3 caractère !";
     isValid = false;
   }else{
     nomMessage.innerText = "";
@@ -52,31 +52,39 @@ let emailUser = document.getElementById("email");
 emailUser.addEventListener("input", function(e){
   let emailMessage = document.getElementById("email-validation");
   if(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(emailUser.value)){
-    emailMessage.innerText = "Valider";
+    emailMessage.innerText = "";
   }else{
-    emailMessage.innerText = "veuiller saisir un mail";
+    emailMessage.innerText = "veuiller saisir un mail "+'"'+" exemple : nom.prenom@gmail.com "+'" !';
     isValid = false;
   }
 });
 
-//verification de la date de naissance
-let jeuneUser = "28-01-2010";
-let deadUser = "28-01-1922";
-let birthdateUser = document.getElementById("birthdate");
+//verification de la date de naissance 
+const todayDate = new Date();
+const oldDate = new Date("01,01,1922")
+const birthdateUser = document.getElementById("birthdate");
+
 birthdateUser.addEventListener("input", function(e){
   let birthdateMessage = document.getElementById("birthdate-valitaion");
-  if(birthdateUser.value < jeuneUser){
-    birthdateMessage.innerText = "Vous êtes trops jeune !";
+  const date1 = todayDate;
+  const date2 = new Date(birthdateUser.value);
+  const date3 = oldDate;
+  console.log(date2);
+  if(date1 > date2 && date2 > date3){
+    birthdateMessage.innerText = "";
+  }else if(date1 < date2){
+    birthdateMessage.innerText = "Impossiqle saisir une année qui est dans le futur";
     isValid = false;
-  }else{
-    birthdateMessage.innerText = " Vous avez l'âge pour participer !";
+  }else if( date2 < date3 ){
+    birthdateMessage.innerText = "Vous êtes trop vieux !";
+    isValid = false;
   }
-})
+});
 
 
-//on récupère l'élément sur lequel on veut vérifier "exemple compter le nombre de lettre"
-/*compteUtilisateur.nom = document.getElementById('last');
-compteUtilisateur.prenom = document.getElementById('first');
-compteUtilisateur.email = document.getElementById('email');
-compteUtilisateur.dateNaissance = document.getElementById('birthdate');*/
 
+
+
+
+/*function validation(valid){
+if(){*/
